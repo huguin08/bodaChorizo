9d8ca8bd0e6debb8486a70e918b15454cf0bab29
huguin08 <humeg1189@gmail.com>
2026-08-26 22:17:15 -0600

    correcion 1 de sobre y animacion

--- js/invitation.js
+++ js/invitation.js
@@ -6,9 +6,45 @@ const openInvitationButton =
 console.log("Botón encontrado:", openInvitationButton);
 
 if (openInvitationButton) {
-    openInvitationButton.addEventListener("click", () => {
-        console.log("Invitación abierta 💍");
-    });
+    let isOpening = false;
+
+    openInvitationButton.addEventListener(
+        "click",
+        () => {
+
+            if (isOpening) {
+                return;
+            }
+
+            isOpening = true;
+
+            console.log(
+                "Abriendo invitación 💍"
+            );
+
+            /*
+             * Paso 1:
+             * desaparecer sello
+             */
+            openInvitationButton.classList.add(
+                "envelope--opening"
+            );
+
+            /*
+             * Paso 2:
+             * abrir solapa
+             */
+            setTimeout(() => {
+
+                openInvitationButton.classList.add(
+                    "envelope--open"
+                );
+
+            }, 350);
+
+        }
+    );
+
 } else {
     console.error("No se encontró el botón #openInvitation");
 }
